6a29558817da368b817dd653e65ac960d8cb396a
Razi Ahmad <raziahmad0789@gmail.com>
2026-04-19 13:56:44 +0530

    Delete 01-Java-Fundamentals/Three-03/ClassAndObjectDemo directory

--- 01-Java-Fundamentals/Three-03/ClassAndObjectDemo/Constructor/ConstructorObject.java
+++
@@ -1,8 +0,0 @@
-package Three.Constructor;
-
-public class ConstructorObject {
-    public static void main(String[] args) {
-        Dog dog=new Dog("Moti","Brown","Barks", 4);
-        dog.activity();
-    }
-}
