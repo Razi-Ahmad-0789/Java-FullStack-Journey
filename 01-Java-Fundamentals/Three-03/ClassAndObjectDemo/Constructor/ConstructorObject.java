91a7c0ea7a39dad8a88b47f6b749fc6f793d9546
Razi Ahmad <raziahmad0789@gmail.com>
2026-04-19 13:53:42 +0530

    Add files via upload
    "Explaination of Constructor

---
+++ 01-Java-Fundamentals/Three-03/ClassAndObjectDemo/Constructor/ConstructorObject.java
@@ -0,0 +1,8 @@
+package Three.Constructor;
+
+public class ConstructorObject {
+    public static void main(String[] args) {
+        Dog dog=new Dog("Moti","Brown","Barks", 4);
+        dog.activity();
+    }
+}
