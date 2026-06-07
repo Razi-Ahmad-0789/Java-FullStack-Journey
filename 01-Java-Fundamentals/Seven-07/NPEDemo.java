3252a5b210a7f70d5c83d8438a811919bd316ff2
Razi Ahmad <raziahmad0789@gmail.com>
2026-06-07 19:38:24 +0530

    Add files via upload

---
+++ 01-Java-Fundamentals/Seven-07/NPEDemo.java
@@ -0,0 +1,50 @@
+package Seven;
+
+import java.util.Optional;
+
+public class NPEDemo {
+    public static void main(String[] args) {
+        String name="Java";
+        if(name!=null)
+            System.out.println(name.length());
+        else
+            System.out.println("No name Value");
+
+        //Creating Optionals
+        Optional<String> optionalString = Optional.of("java");
+        System.out.println(optionalString);
+
+        Optional<String>empty=Optional.empty();
+        System.out.println(empty);
+
+        Optional<String> mayBe = Optional.ofNullable(null);
+        System.out.println(mayBe);
+
+        //Checking Values
+        System.out.println(optionalString.isPresent());
+        System.out.println(mayBe.isPresent());
+
+        System.out.println(optionalString.isEmpty());
+        System.out.println(mayBe.isEmpty());
+
+        //get()
+        System.out.println(optionalString.get());
+//        System.out.println(mayBe.get());
+
+        //orElse("default-value")
+        System.out.println(optionalString.orElse("default"));
+        System.out.println(mayBe.orElse("default"));
+
+        //orElseGet(<supplier>)
+        String result=mayBe.orElseGet(()->{
+            System.out.println("Generating Default Value");
+            return "default";
+        });
+        System.out.println(result);
+        //orElseThrow()
+        String newResult=optionalString.orElseThrow(
+                ()->new RuntimeException("Not Found")
+        );
+        System.out.println(newResult);
+    }
+}
